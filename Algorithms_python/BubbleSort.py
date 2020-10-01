485abe1a21d67c959828d069fcf0f7fb93170fb8
Pranav Tomar <pranav.tomar_cs18@gla.ac.in>
2020-10-01 11:35:50 +0530

    Adding sorting algorithms

---
+++ Algorithms_python/BubbleSort.py
@@ -0,0 +1,9 @@
+
+def Bubble_sort(arr, n):
+    for i in range(0,n-1):
+        for j in range(0,n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+
