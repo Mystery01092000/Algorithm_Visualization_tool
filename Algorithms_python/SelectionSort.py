6280a40156ff0ac32102dd7724c20ebaff459cac
Pranav Tomar <pranav.tomar_cs18@gla.ac.in>
2021-01-10 23:43:24 +0530

    SelectionSort.py added

---
+++ Algorithms_python/SelectionSort.py
@@ -0,0 +1,11 @@
+class SelectionSort:
+
+    def algorithm(self, array):
+        for i in range(len(array)):
+            idx = i
+            for j in range(i+1, len(array)):
+                if array[j] < array[idx]:
+                    idx = j
+
+            array[i], array[idx] = array[idx], array[i]
+
