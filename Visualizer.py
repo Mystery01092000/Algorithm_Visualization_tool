74c97ceaef30ea1d7725a016ebb9dda4271abd34
Pranav Tomar <pranav.tomar_cs18@gla.ac.in>
2021-01-11 00:12:22 +0530

    Working on visualizer.py

--- Visualizer.py
+++ Visualizer.py
@@ -1,11 +1,4 @@
-import random
-import time
-
-from matplotlib import animation
-
-import Algorithms_python
-import matplotlib.pyplot as plt
-from Algorithms_python.BubbleSort import bubble_sort
+import pygame
 
 print("WELCOME TO SORTING VISUALIZER")
 
@@ -19,75 +12,25 @@ print('''
 
 ''')
 
-def swap(A, i, j):
+dimensions = [1024, 512]
 
-    if i != j:
-        A[i], A[j] = A[j], A[i]
+pygame.init()
+display = pygame.display.set_mode((dimensions[0], dimensions[1]))
 
-def bubblesort(A):
+display.fill(pygame.Color("#a48be0"))
 
-    if len(A) == 1:
-        return
+done = False
 
-    swapped = True
-    for i in range(len(A) - 1):
-        if not swapped:
-            break
-        swapped = False
-        for j in range(len(A) - 1 - i):
-            if A[j] > A[j + 1]:
-                swap(A, j, j + 1)
-                swapped = True
-            yield A
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    pygame.display.flip()
 
+
+def visualizer():
+    print("Select an algorithm")
 
 
 if __name__ == "__main__":
-
-    # Take number of integers element
-    n = int(input("Enter the number of elements: "))
-    #print(n)
-    #Create an list of integer elements from 1 to n
-    arr = [i+1 for i in range(n)]
-    #print(arr)
-    #Generate a random number k
-    random.seed(time.time())
-
-    #list is then shuffled k times
-    k = random.randint(0,5) + 1
-    #print(k)
-    for i in range(k):
-        random.shuffle(arr)
-        #print(arr)
-    #print(arr)
-
-    start = time.process_time()
-    generator = bubblesort(arr)
-    print(time.process_time() - start )
-
-
-    fig, ax = plt.subplots()
-    ax.set_title("BubbleSort")
-
-    bar_rects = ax.bar(range(len(arr)), arr, align="edge")
-
-
-    ax.set_xlim(0,n)
-    ax.set_ylim(0, int(1.07 * n))
-
-    text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
-
-    iteration = [0]
-
-
-    def update_fig(A, rects, iteration):
-        for rect, val in zip(rects, A):
-            rect.set_height(val)
-        iteration[0] += 1
-        text.set_text("# of operations: {}".format(iteration[0]))
-
-
-    anim = animation.FuncAnimation(fig, func=update_fig,
-                                   fargs=(bar_rects, iteration), frames=generator, interval=1,
-                                   repeat=False)
-    plt.show()
+    visualizer()
