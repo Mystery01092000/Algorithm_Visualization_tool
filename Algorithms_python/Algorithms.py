aab77cd7885d57928277a43df17e0ba0c24fbaa2
Pranav Tomar <pranav.tomar_cs18@gla.ac.in>
2021-01-11 20:27:59 +0530

    visualizer updated

---
+++ Algorithms_python/Algorithms.py
@@ -0,0 +1,21 @@
+from datetime import time
+from random import random
+
+
+class Algorithms:
+
+    def __init__(self, name):
+        self.array = random.sample(range(512), 512)
+        self.name = name
+
+    def update_display(self, swap1=None, swap2=None):
+            import visualizer
+            visualizer.update(self, swap1, swap2)
+
+    def run(self):
+        self.start_time = time.time()
+        self.algorithms()
+        time_elapsed = time.time() - self.start_time
+        return self.array, time_elapsed
+
+
--- Visualizer.py
+++ Visualizer.py
@@ -1,4 +1,14 @@
+import sys
+from datetime import time
+
 import pygame
+
+from Algorithms_python import Algorithms
+from Algorithms_python.BubbleSort import BubbleSort
+from Algorithms_python.InsertionSort import InsertionSort
+from Algorithms_python.MergeSort import MergeSort
+from Algorithms_python.QuickSort import QuickSort
+from Algorithms_python.SelectionSort import SelectionSort
 
 print("WELCOME TO SORTING VISUALIZER")
 
@@ -14,22 +24,48 @@ print('''
 
 dimensions = [1024, 512]
 
+algorithms = {"SelectionSort": SelectionSort(), "BubbleSort": BubbleSort(), "InsertionSort": InsertionSort(), "MergeSort": MergeSort(), "QuickSort": QuickSort()}
+
 pygame.init()
 display = pygame.display.set_mode((dimensions[0], dimensions[1]))
 
 display.fill(pygame.Color("#a48be0"))
 
-done = False
+if len(sys.argv) > 1:
+    if sys.argv[1] == "list":
+        for key in algorithms.keys(): print(key, end=" ")
+        print("")
+        sys.exit(0)
 
-while not done:
+def check_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = True
-    pygame.display.flip()
+            pygame.quit();
+            sys.exit();
 
+def update(algorithm, swap1=None, swap2=None, display=display):
+    display.fill(pygame.Color("#a48be0"))
+    pygame.display.set_caption("Sorting Visualizer     Algorithm: {}     Time: {:.3f}      Status: Sorting...".format(algorithm.name, time.time() - algorithm.start_time)) # Display on title bar
+    k = int(dimensions[0]/len(algorithm.array))
+    for i in range(len(algorithm.array)):
+        colour = (80, 0, 255)
+        if swap1 == algorithm.array[i]:
+            colour = (0,255,0)
+        elif swap2 == algorithm.array[i]:
+            colour = (255,0,0)
+        pygame.draw.rect(display, colour, (i*k,dimensions[1],k,-algorithm.array[i]))
+    check_events()
+    pygame.display.update()
+
+def keep_open(algorithm, display, time):
+    pygame.display.set_caption("Sorting Visualizer     Algorithm: {}     Time: {:.3f}      Status: Done!".format(algorithm.name, time))
+    while True:
+        check_events()
+        pygame.display.update()
 
 def visualizer():
     print("Select an algorithm")
+
 
 
 if __name__ == "__main__":
